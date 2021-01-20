c2bb639b64080a4880bad61915e5ee9b0ee1e15a
HLJ <2624672807@qq.com>
2021-01-20 15:07:53 +0800

    HLJ

---
+++ src/main/java/org/accp/procurement/mapper/ProductfileMapper.java
@@ -0,0 +1,10 @@
+package org.accp.procurement.mapper;
+
+import org.accp.procurement.entity.Productfile;
+
+import java.util.List;
+
+public interface ProductfileMapper {
+
+    List<Productfile> selectProduct();
+}
