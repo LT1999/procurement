82883d749ee738baa5c16f4bb43853f30dabff18
LT <luotai1999@163.com>
2021-01-21 10:05:17 +0800

    lt

--- src/main/java/org/accp/procurement/mapper/ProductfileMapper.java
+++
@@ -1,9 +0,0 @@
-package org.accp.procurement.mapper;
-
-import org.accp.procurement.entity.Productfile;
-import java.util.List;
-
-public interface ProductfileMapper {
-
-    List<Productfile> selectProduct();
-}
